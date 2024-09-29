2d531e0004b2874f28139cd4d5161d35d8b401ff
Mustapha Suleiman <mustapha.suleiman@veristein.com>
2024-09-29 16:54:05 +0100

    changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,28 @@
 import "@styles/globals.css";
-import { Children } from "react";
 
-export const metadate = {
+// import Nav from "@components/Nav";
+// import Provider from "@components/Provider";
+
+export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
-const RootLayout = () => {
-  return (
-    <html lang="en">
-      <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">{children}</main>
-      </body>
-    </html>
-  );
-};
+
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      {/* <Provider> */}
+      <div className="main">
+        <div className="gradient" />
+      </div>
+
+      <main className="app">
+        {/* <Nav /> */}
+        {children}
+      </main>
+      {/* </Provider> */}
+    </body>
+  </html>
+);
 
 export default RootLayout;
